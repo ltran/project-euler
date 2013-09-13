b80a9563b4b12b5e11cca44187f06f1207747226
Louis Tran <louis@razoo.com>
2013-09-13 00:36:27 -0700

    remove comments

--- spec/problem_1_spec.rb
+++ spec/problem_1_spec.rb
@@ -1,11 +1,5 @@
 require './lib/problem_1.rb'
 
-
-#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
-#Find the sum of all the multiples of 3 or 5 below 1000.
-
-
 describe 'Natural numbers' do
   context 'brute way' do
     it 'should sum all the multiples of 3 or 5 below 10' do
